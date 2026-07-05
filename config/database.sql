6aa0744210d483d0853ec78a8466b89c2580d545
Heriansyah Sutrisno <heriansyah0327@gmail.com>
2026-07-05 16:39:34 +0700

    Harimaw Commit

--- config/database.sql
+++ config/database.sql
@@ -1,21 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jul 05, 2026 at 04:20 PM
--- Server version: 5.7.44-cll-lve
--- PHP Version: 7.2.34
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `heriansyah_gudang`

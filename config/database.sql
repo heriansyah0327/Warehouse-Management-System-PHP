0df0c1cccc9fdfd5077e2a21ccd3f2f62277a152
Heriansyah Sutrisno <heriansyah0327@gmail.com>
2026-07-05 16:38:36 +0700

    Update database.sql

--- config/database.sql
+++ config/database.sql
@@ -291,8 +291,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `full_name`, `role`, `created_at`) VALUES
-(1, 'admin', 'pepekmemek002', 'Admin Gudang', 'admin', '2026-07-04 05:37:46'),
-(4, 'staff', 'staff', 'Staff Gudang', 'staff', '2026-07-04 14:53:32');
+(1, 'admin', 'admin123', 'Admin Gudang', 'admin', '2026-07-04 05:37:46'),
+(4, 'staff', 'staff123', 'Staff Gudang', 'staff', '2026-07-04 14:53:32');
 
 --
 -- Indexes for dumped tables
